e3c75f77294a346ca4fdc878e00008f45d0a859a
hq <hq@hq.com>
2022-09-07 11:37:06 +0800

    first commit

---
+++ src/main/java/com/hq/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.hq.git;
+
+/**
+ * @author 韩琪
+ * @version 1.0
+ * 2022/9/7 11:35
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git!");
+    }
+}
